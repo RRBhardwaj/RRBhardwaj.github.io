ba5990eda299e43804663b6b1818c5066ebf1eb3
Rishi <rishirajbhardwaj20@gmail.com>
2022-12-31 02:07:22 +0530

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import './App.css';
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <Navbar/>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#1a0207] text-gray-300'>
+        <div>
+            <h4>Rishi Bhardwaj</h4>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
