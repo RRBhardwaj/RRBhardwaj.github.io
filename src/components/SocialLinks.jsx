68866ead259232b2858ef31ec6e68e1b9cf752de
Rishi <rishirajbhardwaj20@gmail.com>
2023-01-02 22:25:04 +0530

    Social Media

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -8,22 +8,22 @@ const SocialLinks = () => {
     <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
             <ul>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500'>
-                    <a className='flex justify-between items-center w-full text-gray-300' href="/">Linkedin<FaLinkedin size={30}/></a>
+                    <a className='flex justify-between items-center w-full text-gray-300' href="https://linkedin.com" target="_blank" rel="noreferrer">Linkedin<FaLinkedin size={30}/></a>
                 </li>
             </ul>
             <ul>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#171616]'>
-                    <a className='flex justify-between items-center w-full text-gray-300' href="/">Github<FaGithub size={30}/></a>
+                    <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/RRBhardwaj" target="_blank" rel="noreferrer">Github<FaGithub size={30}/></a>
                 </li>
             </ul>
             <ul>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#53ac99]'>
-                    <a className='flex justify-between items-center w-full text-gray-300' href="/">Email<HiOutlineMail size={30}/></a>
+                    <a className='flex justify-between items-center w-full text-gray-300' href="mailto: rishirajbhardwaj20@gmail.com" target="_blank" rel="noreferrer">Email<HiOutlineMail size={30}/></a>
                 </li>
             </ul>
             <ul>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#2f3133]'>
-                    <a className='flex justify-between items-center w-full text-gray-300' href="/">Resume<BsFillPersonLinesFill size={30}/></a>
+                    <a className='flex justify-between items-center w-full text-gray-300' href="/Rishi_Bhardwaj_Resume.pdf" target="_blank" rel="noreferrer" download={true}>Resume<BsFillPersonLinesFill size={30}/></a>
                 </li>
             </ul>
         </div>
