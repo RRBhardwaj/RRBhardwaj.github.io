e193597ff9e42c8be86620728eedbcfe3eec36a4
Rishi <rishirajbhardwaj20@gmail.com>
2025-07-02 07:47:56 +0530

    Changes Google Drive Link and Resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     }
     
     const handleResume = () => {
-        window.open('https://drive.google.com/file/d/1jFPUwmPU2j21g5YDqNi74oJauCHcLfIE/view?usp=sharing', '_blank');
+        window.open('https://drive.google.com/file/d/1PtlxVaHPxFkmHIUh_ujZ0nJ7o_-eUarA/view?usp=sharing', '_blank');
         const link = document.createElement('a');
         link.download = 'Rishi_Bhardwaj_Resume.pdf';
         document.body.appendChild(link);
@@ -17,7 +17,7 @@ const Navbar = () => {
       }
 
     const mobileResume = () => {
-        window.open('https://drive.google.com/file/d/1jFPUwmPU2j21g5YDqNi74oJauCHcLfIE/view?usp=sharing', '_blank');
+        window.open('https://drive.google.com/file/d/1PtlxVaHPxFkmHIUh_ujZ0nJ7o_-eUarA/view?usp=sharing', '_blank');
         const link = document.createElement('a');
         link.download = 'Rishi_Bhardwaj_Resume.pdf';
         document.body.appendChild(link);
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -5,7 +5,7 @@ import {BsFillPersonLinesFill} from "react-icons/bs";
 
 const SocialLinks = () => {
     const handleClick = () => {
-        window.open('https://drive.google.com/file/d/1jFPUwmPU2j21g5YDqNi74oJauCHcLfIE/view?usp=sharing', '_blank');
+        window.open('https://drive.google.com/file/d/1PtlxVaHPxFkmHIUh_ujZ0nJ7o_-eUarA/view?usp=sharing', '_blank');
         const link = document.createElement('a');
         link.download = 'Rishi_Bhardwaj_Resume.pdf';
         document.body.appendChild(link);
